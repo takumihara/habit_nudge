5e138b24641bab808fdca5d61d9b1e36d593885f
Takumi Hara <takumihara1226@gmail.com>
2025-03-06 00:02:50 +0900

    Add icon

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    icon: path.join(__dirname, "icon.png"),
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
